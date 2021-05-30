0edb1aef184efac2a1d3a69ab4c71b5977bfeb0a
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-30 02:53:44 +0000

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <Container flex>&copy; Tees.Design LLC{{ new Date().getFullYear() }}</Container>
+    <Container flex>&copy; Tees.Design LLC {{ new Date().getFullYear() }}</Container>
   </footer>
 </template>
 
